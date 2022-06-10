60db05a12c08d09f53aab83efedddbbbb7ba93b7
Louis H <94133351+ssuilszV@users.noreply.github.com>
2022-06-10 15:02:41 -0300

    Add files via upload

---
+++ bloco.js
@@ -0,0 +1,20 @@
+{ 
+    console.log("Passo #1");
+    console.log("Passo #2");
+    console.log("Passo #3");
+    console.log("Passo #4");
+}
+
+{
+    console.log("Passo #5");
+    console.log("Passo #6");
+}
+
+//Esse bloco não tem conteudo, logo nada irá ler-lo
+{
+    ;
+    ;
+    ;
+}
+
+console.log("Fim!");
